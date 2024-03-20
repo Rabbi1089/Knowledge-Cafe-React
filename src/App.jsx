5d6f3ad97ee7f2ad0afab0c740ac31d3bc0973b8
Rabbi1089 <rabbi.ext@gmail.com>
2024-03-20 16:21:41 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+    <div className='border-8 bg-green-200 w-full h-28'>Hi  </div>
+    <h1 className='text-6xl text-green-700 bg-slate-400 text-center p-8'>
+      Hello world!
+    </h1>
+  
+    
+    </>
+  )
+}
+
+export default App
